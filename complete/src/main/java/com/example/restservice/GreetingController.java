bb3eb864bdeecd700295699558a079940c8e02d4
schillingerf <schillingerf@gmail.com>
2020-08-24 20:34:22 +0000

    init

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -5,15 +5,26 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.io.File;
+import org.apache.commons.io.FileUtils;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private  String template = "junk";
+	private  AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		try {
+			if (template.length() == 0) {
+			File file = new File("/tmp/fixed_1000.json");
+			template = FileUtils.readFileToString(file);
+			System.out.println("Read in " + template.length());
+			return template;
+		} else {
+		return template;
+		}
+		} catch (Exception e) {
+		}
 	}
 }
